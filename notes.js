2f41024f88eb5c29f6043427b732c98191eb8cf9
chesahkalu <chesahkalu@gmail.com>
2023-07-20 19:41:10 +0100

    loops

--- notes.js
+++ notes.js
@@ -1,23 +1,9 @@
-// While loop : runs while a specified condition is true and stops once that condition is no longer true.
-
-const myArray = [];
-
-let i = 5
-
-while (i >= 0){
-  myArray.push(i);
-  i--;
+function sum(arr, n) {
+  // Only change code below this line
+ if (n <= 0) {
+      return 0;
+    } else {
+      return sum(arr, n - 1) + arr[n - 1];
+    }
+  // Only change code above this line
 }
-
-// For loop : runs for a specific number of times and stops once that number of times is reached.
-// Declared with three optional expressions separated by semicolons: for ([initialization]; [condition]; [final-expression])
-// The initialization statement is executed one time only before the loop starts. It is typically used to define and setup your loop variable.
-// The condition statement is evaluated at the beginning of every loop iteration and will continue as long as it evaluates to true. When condition is false at the start of the iteration, the loop will stop executing. This means if condition starts as false, your loop will never execute.
-// The final-expression is executed at the end of each loop iteration, prior to the next condition check and is usually used to increment or decrement your loop counter.
-
-const herArray = [];
-
-for (let k = 0; k <= 10; k +=2){  //
-  herArray.push(k);
-}
-console.log(herArray);
