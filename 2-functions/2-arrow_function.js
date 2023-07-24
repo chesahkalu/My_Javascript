c186b1c3b48dd461d53c636601bec6940990b155
chesahkalu <chesahkalu@gmail.com>
2023-07-24 14:48:13 +0100

    function expression and arrow functions

--- 2-functions/2-arrow_function.js
+++ 2-functions/2-arrow_function.js
@@ -17,7 +17,6 @@ const theFunc = () => {
 
 
 //When there is no function body, and only a return value, arrow function syntax allows you to omit the keyword return as well as the brackets surrounding the code. This helps simplify smaller functions into one-line statements:
-
 const myFunc = () => "value"; //This code will still return the string value by default.
 
 
@@ -28,7 +27,6 @@ console.log(magic());
 
 
 //Just like a regular function, you can pass arguments into an arrow function.
-
 const doubler = (item) => item * 2;
 doubler(4); //return the value 8.
 
@@ -38,9 +36,21 @@ const myDoubler = item => item * 2;
 
 
 //It is possible to pass more than one argument into an arrow function.
-
 const multiplier = (item, multi) => item * multi;
 multiplier(4, 2); //return the value 8.
 
 const myConcat = (arr1, arr2) => arr1.concat(arr2); //This is an arrow function that returns the concatenated array.
 console.log(myConcat([1, 2], [3, 4, 5])); // prints [1, 2, 3, 4, 5]
+
+
+
+
+//In order to help us create more flexible functions, ES6 introduces default parameters for functions.
+const greeting = (name = "Anonymous") => "Hello " + name;
+
+console.log(greeting("John")); // prints Hello John
+console.log(greeting());  // prints Hello Anonymous
+
+/*The default parameter kicks in when the argument is not specified (it is undefined). As you can see in the example above,
+the parameter name will receive its default value Anonymous when you do not provide a value for the parameter. You can add default values for as many parameters as you want*/
+
