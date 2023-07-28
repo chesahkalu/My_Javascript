3e33beeb3fea9211dfc69c42b67613c550217f83
chesahkalu <chesahkalu@gmail.com>
2023-07-28 13:24:17 +0100

    template literals

--- 1-beginer_basics/2-strings.js
+++ 1-beginer_basics/2-strings.js
@@ -36,7 +36,7 @@ const myStr = "This is the start. " + "This is the end.";
 // Concatenating strings with the += operator
 let myStrr = "This is the first sentence. ";
 
-myStr += "This is the second sentence."; // this is possibe with a string variable
+myStrr += "This is the second sentence."; // this is possibe with a string variable
 
 
 const myName = "Vee";
@@ -65,3 +65,53 @@ secondToLastLetterOfLastName = lastName[lastName.length - 2]; // secondToLastLet
 let immStr = "Jello World";
 immStr[0] = "H"; // This is not possible, it will not change the value of myStr
 immStr = "Hello World"; // This is possible, it will change the value of myStr
+
+
+//Template literals - Template literals allow you to create multi-line strings and to use string interpolation features to create strings.
+const person = {
+    name: "Zodiac Hasbro",
+    age: 56
+    };
+
+const greeting = `Hello, my name is ${person.name}! 
+I am ${person.age} years old.`;
+
+console.log(greeting); // Prints Hello, my name is Zodiac Hasbro! I am 56 years old.
+/*A lot of things happened there. Firstly, the example uses backticks (`), not quotes (' or "), to wrap the string. This is the basic feature of a template literal. It makes creating complex strings easier.
+Secondly, notice that the string is multi-line, both in the code and the output. This saves inserting \n within strings. with the (`) , the string can be written in multiple lines, as it is in the code.
+The ${variable} syntax used above is a placeholder. Basically, you won't have to use concatenation with the + operator anymore. 
+To add variables to strings, you just drop the variable in a template string and wrap it with ${ and }. 
+Similarly, you can include other expressions in your string literal, for example ${a + b}. 
+This new way of creating strings gives you more flexibility to create robust strings. */
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    "use strict";
+   
+    const failureItems = [];
+    for (let i = 0; i < arr.length; i++) {
+      failureItems.push(`this is it = ${arr[i]}</li>`); //with the (`), the strings will come out in multiple lines, as it is in the code.
+    }
+   
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
+  console.log(failuresList);
+
+
+//You can also use template literals for multi-line strings. You can do this by adding a backslash (\) at the end of the first line.
+const str = `This is the first line \
+This is the second line \
+This is the third line`;
+
+console.log(str); // Prints This is the first line This is the second line This is the third line
+
+
+
+
+
