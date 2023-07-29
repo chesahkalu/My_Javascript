6ee75a4547aceded01c70140421ffe28b23c5926
chesahkalu <chesahkalu@gmail.com>
2023-07-29 06:55:17 +0100

    regex

--- 9-regular_expresions/1-regex.js
+++ 9-regular_expresions/1-regex.js
@@ -53,6 +53,11 @@ Character classes allow you to define a group of characters you wish to match by
 For example, you want to match bag, big, and bug but not bog. You can create the regex /b[aiu]g/ to do this. The [aiu] is the character class that will only match the characters a, i, or u. */
 
 let bgRegex = /b[aiu]g/; // This is the regex. The [aiu] is the character class that will only match the characters a, i, or u.
+let bag = "bag";
+let big = "big";
+let bug = "bug";
+let bog = "bog";
+
 console.log(bag.match(bgRegex)); // This will return ["bag"].
 console.log(big.match(bgRegex)); // This will return ["big"].
 console.log(bug.match(bgRegex)); // This will return ["bug"].
@@ -61,4 +66,30 @@ console.log(bog.match(bgRegex)); // This will return null.
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi; // This is the regex. The [aeiou] is the character class that will only match the characters a, e, i, o, or u.
 console.log(quoteSample.match(vowelRegex)); // This will return ["e", "a", "e", "o", "u", "i", "e", "a", "o", "e", "o", "e", "I", "a", "e", "o", "o", "e", "i", "o", "e", "o", "i"].
+
+/* Match Letters of the Alphabet, You saw how you can use character sets to specify a group of characters to match, but that's a lot of typing when you need to match a large range of characters(for example, every letter in the alphabet).
+Fortunately, there is a built-in feature that makes this short and simple. Inside a character set, you can define a range of characters to match using a hyphen character: -. */
+// For example, to match lowercase letters a through e you would use [a-e].
+
+let catStr = "cat"; 
+let batStr = "bat";
+let matStr = "mat";
+console.log(catStr.match(/[a-e]at/)); // This will return ["cat"].
+console.log(batStr.match(/[a-e]at/)); // This will return ["bat"].
+console.log(matStr.match(/[a-e]at/)); // This will return null.
+
+/* Match Numbers and Letters of the Alphabet, Using the hyphen (-) to match a range of characters is not limited to letters. It also works to match a range of numbers.
+For example, /[0-5]/ matches any number between 0 and 5, including the 0 and 5. Also, it is possible to combine a range of letters and numbers in a single character set. */
+
+let jennyStr = "Jenny8675309";
+let myRegex1 = /[a-z0-9]/ig; // This is the regex. The [a-z0-9] is the character class that will only match the characters a through z and 0 through 9.
+console.log(jennyStr.match(myRegex1)); // This will return ["J", "e", "n", "n", "y", "8", "6", "7", "5", "3", "0", "9"].
+
+/* Match Single Characters Not Specified, So far, you have created a set of characters that you want to match, but you could also create a set of characters that you do not want to match.
+These types of character sets are called negated character sets. To create a negated character set, you place a caret character (^) after the opening bracket and before the characters you do not want to match.
+For example, /[^aeiou]/gi matches all characters that are not a vowel. Note that characters like ., !, [, @, / and white space are matched - the negated vowel character set only excludes the vowel characters. */
+
+let quoteSample1 = "3 blind mice.";
+let myRegex2 = /[^aeiou0-9]/gi; // This is the regex. The [^aeiou0-9] is the negated character class that will only match the characters that are not a vowel and not a number.
+console.log(quoteSample1.match(myRegex2)); // This will return [" ", "b", "l", "n", "d", " ", "m", "c", "."].
 
