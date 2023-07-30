190d87ea519e2c396e39c0a6ad94969c7610467f
chesahkalu <chesahkalu@gmail.com>
2023-07-30 14:47:50 +0100

    arrays

--- 4-arrays/4-array_functions.js
+++ 4-arrays/4-array_functions.js
@@ -29,4 +29,18 @@ function greaterThanTen(arr) {
   }
   
 greaterThanTen([2, 12, 8, 14, 80, 0, 1]);
-  
+
+// Function that checks if an element is in the arrays withing an array, and if the element is not present, it returns a new array with the arrays that do not contain the element.
+function filteredArray(arr, elem) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) { //Iterates through the array of arrays
+      if (arr[i].indexOf(elem) == -1) { //Checks the array for the element and if is NOT there continues the code
+        newArr.push(arr[i]); //Inserts the element of the array in the new filtered array
+      }
+    }
+    return newArr;
+    console.log(newArr);
+  }
+
+  console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9], [4, 5, 6]], 3));
